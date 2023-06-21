870642ec88d276d8060fdab39715e851f22f154e
jadestern <prelasia@gmail.com>
2023-06-21 22:45:51 +0900

    tailwind 설정 추가

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,13 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<h1>hello</h1>
+<h1 class="text-3xl font-bold underline">
+	Hello world!
+</h1>
+
+
+<style lang="postcss">
+	:global(html) {
+			background-color: theme('colors.gray.100');
+	}
+</style>

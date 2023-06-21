bdf7508826a6ad079c69aa8d0f67380a46305915
jadestern <prelasia@gmail.com>
2023-06-21 23:10:39 +0900

    Hero 추가 및 적용

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,22 @@
-<script></script>
+<script>
+  import Hero from "./Hero.svelte";
+</script>
 
 <svelte:head>
 	<title>Koreans do</title>
-	<meta name="description" content="Svelte demo app" />
+	<meta content="Svelte demo app" name="description" />
 </svelte:head>
 
-<h1 class="text-3xl font-bold underline">
-	Hello world!
-</h1>
-
+<section>
+	<Hero
+		buttonLabel="Get Started"
+		description="Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi."
+		title='Hello there'
+	/>
+</section>
 
 <style lang="postcss">
 	:global(html) {
-			background-color: theme('colors.gray.100');
+		background-color: theme('colors.gray.100');
 	}
 </style>
